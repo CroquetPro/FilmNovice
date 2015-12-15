dee3ecf3d1ef30ee188008ab70af94ebdb73ef1c
Ben Rothman <croquetpro@gmail.com>
2015-12-15 15:37:33 -0800

    react router added navbar built frontend folders and some css styling

---
+++ frontend/components/header/navbar.jsx
@@ -0,0 +1,16 @@
+var React = require('react');
+
+var NavBar = React.createClass({
+  render: function(){
+    // <Search />
+    // <User />
+    // <Errors />
+    return(
+      <div className='NavBar'>
+        <h1>Film Novice</h1>
+      </div>
+    );
+  }
+});
+
+module.exports = NavBar;
--- frontend/film_novice.jsx
+++ frontend/film_novice.jsx
@@ -2,14 +2,15 @@ var React = require('react'),
     ReactDOM = require('react-dom'),
     Router = require('react-router').Router,
     Route = require('react-router').Route,
-    IndexRoute = require('react-router').IndexRoute;
+    IndexRoute = require('react-router').IndexRoute,
+    NavBar = require('./components/header/navbar');
 
 document.addEventListener('DOMContentLoaded', function () {
   var App = React.createClass({
     render: function(){
       return (
         <div>
-          // <NavBar />
+          <NavBar />
           {this.props.children}
         </div>
       );
@@ -17,11 +18,11 @@ document.addEventListener('DOMContentLoaded', function () {
   });
   var routes = (
     <Route path="/" component={App} >
-      // <IndexRoute component={MoviesIndex} ></IndexRoute>
-      // <Route path="movies/new" component={MovieForm} />
-      // <Route path="movies/:movieId" component={MovieShow} />
-      // <Route path="users/:userId" component={UserShow} />
     </Route>);
   var content = document.querySelector('#content');
   ReactDOM.render(<Router>{routes}</Router>, content);
 });
+// <IndexRoute component={MoviesIndex} ></IndexRoute>
+// <Route path="movies/new" component={MovieForm} />
+// <Route path="movies/:movieId" component={MovieShow} />
+// <Route path="users/:userId" component={UserShow} />
